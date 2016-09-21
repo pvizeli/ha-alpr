7d16a6b01084fbc347745226eff62e89a79e4590
pvizeli <pvizeli@syshack.ch>
2016-09-21 17:40:49 +0200

    fix test

--- test.py
+++ test.py
@@ -5,6 +5,5 @@ alpr = HAAlpr()
 with open("test.jpg", "rb") as fl_image:
     image = fl_image.read()
 
-alpr.recognize_byte(image)
+result = alpr.recognize_byte(image)
 print("%s" % result)
-loop.close()
